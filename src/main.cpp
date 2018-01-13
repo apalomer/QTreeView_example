f3bf0770805e18252c89bc472477004cb2e75c80
Albert Palomer <albert.palo@gmail.com>
2018-01-13 20:38:59 +0100

    QTreeView_example

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <QtGui/QApplication>
+#include "QMainWidget.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app (argc, argv);
+    QMainWidget mw;
+    mw.show();
+    return app.exec();
+}
